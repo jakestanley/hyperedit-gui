5d7d5eab84ccdfcb2cc9946a34bfde233a1d9db8
Jacob Stanley <>
2024-06-11 23:25:12 +0100

    tracks window done more or less

---
+++ hyperedit_gui/view/tracks_window.py
@@ -0,0 +1,127 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenuBar, QMenu, QLabel, QWidget, QHBoxLayout, QCheckBox, QVBoxLayout, QListWidget, QListWidgetItem, QGroupBox, QPushButton, QLineEdit
+from PySide6.QtGui import QAction, QDoubleValidator
+from PySide6.QtCore import QCoreApplication, Qt
+
+class TrackWidget(QWidget):
+    def __init__(self, index, enabled):
+        super().__init__()
+        self.index = index
+        self.enabled = enabled
+        self.initUI()
+
+    def initUI(self):
+        hLayout = QHBoxLayout(self)
+        hLayout.setContentsMargins(8, 8, 8, 8)
+        hLayout.addWidget(QLabel(f"Track {self.index}"))
+        self.checkbox = QCheckBox()
+        self.checkbox.setChecked(self.enabled)
+        hLayout.addStretch()
+        hLayout.addWidget(self.checkbox, alignment=Qt.AlignRight)
+        previewButton = QPushButton("Preview")
+        hLayout.addWidget(previewButton, alignment=Qt.AlignRight)
+        self.setLayout(hLayout)
+
+class TracksWindow(QMainWindow):
+    def __init__(self, tracks=[]):
+        super().__init__()
+
+        self.tracks = tracks
+
+        # Set the main window's size
+        self.resize(600, 480)
+
+        self.central_widget = QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.layout = QVBoxLayout(self.central_widget)
+
+        self.layout.addWidget(self.create_tracks_groupbox())
+
+        self.populateList()
+
+    def get_tracks_bitmap(self):
+        bitmap = 0
+        for index, value in enumerate(self.tracks):
+            if value:
+                bitmap |= (1 << index)
+        return bitmap
+    
+    def create_tracks_groupbox(self) -> QGroupBox:
+        groupbox = QGroupBox("Tracks")
+        vlayout = QVBoxLayout()
+        
+        self.listWidget = QListWidget()
+        vlayout.addWidget(self.listWidget)
+
+        # merge audio tracks layout
+        hlayout = self.create_merge_tracks_hlayout()
+        vlayout.addLayout(hlayout)
+
+        # transcribe audio tracks layout
+        hlayout = self.create_transcribe_hlayout(hlayout)
+        vlayout.addLayout(hlayout)
+
+        # deaggress layout
+        hlayout = self.create_deaggress_hlayout()
+        vlayout.addLayout(hlayout)
+
+        groupbox.setLayout(vlayout)
+        return groupbox
+
+    def create_merge_tracks_hlayout(self):
+        hlayout = QHBoxLayout()
+        merge_button = QPushButton("Merge")
+        merge_button.setMaximumWidth(80)
+        hlayout.addWidget(merge_button)
+        # merge label display warning if file already exists
+        self.merge_label = QLabel("⚠️")
+        hlayout.addWidget(self.merge_label)
+        self.merge_path_line_edit = QLineEdit(f"blah{self.get_tracks_bitmap()}.wav")
+        self.merge_path_line_edit.setEnabled(False)
+        hlayout.addWidget(self.merge_path_line_edit)
+        return hlayout
+
+    def create_transcribe_hlayout(self, hlayout):
+        hlayout = QHBoxLayout()
+        transcribe_button = QPushButton("Transcribe")
+        transcribe_button.setEnabled(False)
+        hlayout.addWidget(transcribe_button)
+        # transcribe label displays warning if merged audio does not exist
+        self.transcribe_label = QLabel("⚠️")
+        hlayout.addWidget(self.transcribe_label)
+        self.transcribe_path_line_edit = QLineEdit(f"blah{self.get_tracks_bitmap()}.srt")
+        self.transcribe_path_line_edit.setEnabled(False)
+        hlayout.addWidget(self.transcribe_path_line_edit)
+        return hlayout
+
+    def create_deaggress_hlayout(self):
+        hlayout = QHBoxLayout()
+        deaggress_button = QPushButton("Deaggress")
+        self.deaggress_seconds_line_edit = QLineEdit(self)
+        self.deaggress_seconds_line_edit.setText("1.0")
+        
+        # deaggress seconds validator
+        validator = QDoubleValidator(0.1, 60.0, 1, self)
+        validator.setNotation(QDoubleValidator.StandardNotation)
+        self.deaggress_seconds_line_edit.setValidator(validator)
+        hlayout.addWidget(deaggress_button)
+        hlayout.addWidget(QLabel("by seconds"))
+        hlayout.addWidget(self.deaggress_seconds_line_edit)
+        return hlayout
+
+    def populateList(self):
+        for i in range(0, len(self.tracks)):
+            listItem = QListWidgetItem(self.listWidget)
+            trackWidget = TrackWidget(i, self.tracks[i])
+            listItem.setSizeHint(trackWidget.sizeHint())
+            listItem.setFlags(listItem.flags() & ~Qt.ItemIsSelectable)
+            self.listWidget.addItem(listItem)
+            self.listWidget.setItemWidget(listItem, trackWidget)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # QCoreApplication.setApplicationName("HyperEdit")
+    window = TracksWindow(tracks=[True, False, True])
+    window.show()
+    sys.exit(app.exec())
+    
